449c5da7299299db1a9231c3c3846716e58ea2c7
daum7766 <daum7766@naver.com>
2019-08-20 01:04:59 +0900

    퀵정렬 수정

--- 데이터정렬(1452번)/Large_Sort(Quick_Sort).cpp
+++ 데이터정렬(1452번)/Large_Sort(Quick_Sort).cpp
@@ -3,56 +3,54 @@
 
 using namespace std;
 
+list<int> a;
+int lenth = 0;
 //퀵정렬 함수
-list<int> Quick_sort(list<int> b, int t_lenth)
+list<int> Quick_sort(list<int> b)
 {
-	//재귀함수 탈출조건
-	if (t_lenth <= 1)
+	//재귀함수 리턴조건
+	if (b.size() <= 1)
 	{
 		return b;
 	}
-	//기준점을 꺼내고 크기감소
+	//기준점 설정
 	int pivot = *b.begin();
 	b.pop_front();
-	t_lenth--;
-	//임시 리스트 설정
+	int t_lenth = b.size();
+	//임시변수
 	list<int> t_a;
-	int t_a_Lenth = 0;
 	list<int> t_b;
-	int t_b_Lenth = 0;
-	//기준점과 비교하여 분리
+	//크기 비교후 분할
 	for (int i = 0; i < t_lenth; i++)
 	{
 		if (pivot >= *b.begin())
 		{
 			t_a.push_back(*b.begin());
 			b.pop_front();
-			t_a_Lenth++;
 		}
 		else
 		{
 			t_b.push_back(*b.begin());
 			b.pop_front();
-			t_b_Lenth++;
 		}
 	}
 	//재귀함수 호출
-	t_a = Quick_sort(t_a, t_a_Lenth);
-	t_b = Quick_sort(t_b, t_b_Lenth);
-	//병합과정
+	t_a = Quick_sort(t_a);
+	t_b = Quick_sort(t_b);
+	//정렬된 값 합치기
 	t_a.push_back(pivot);
-	for (int i = 0; i < t_b_Lenth; i++)
+	t_lenth = t_b.size();
+	for (int i = 0; i < t_lenth; i++)
 	{
 		t_a.push_back(*t_b.begin());
 		t_b.pop_front();
 	}
+	//정렬된 결과값 반환
 	return t_a;
 }
 
 int main()
 {
-	list<int> a;
-	int lenth = 0;
 	cin >> lenth;
 	int t;
 	for (int i = 0; i < lenth; i++)
@@ -60,7 +58,7 @@ int main()
 		cin >> t;
 		a.push_back(t);
 	}
-	a = Quick_sort(a, lenth);
+	a = Quick_sort(a);
 	for (int i = 0; i < lenth; i++)
 	{
 		cout << *a.begin() << endl;
