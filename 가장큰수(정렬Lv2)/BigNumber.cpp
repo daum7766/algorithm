6110f0066876a90b3f989a883eebd0e4fe222390
daum7766 <daum7766@naver.com>
2019-10-02 21:57:40 +0900

    정렬 2Lv 가장큰수 추가

---
+++ 가장큰수(정렬Lv2)/BigNumber.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(string a, string b)
+{
+	return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	vector<string> temp;
+	for (auto num : numbers)
+	{
+		temp.push_back(to_string(num));
+	}
+	sort(temp.begin(), temp.end(), cmp);
+	if (temp.at(0) == "0")
+	{
+		return "0";
+	}
+	for (auto num : temp)
+	{
+		answer += num;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	vector<int> b = { 3, 30, 34, 5, 9 };
+	cout << solution(b) << endl;
+
+	return 0;
+}
