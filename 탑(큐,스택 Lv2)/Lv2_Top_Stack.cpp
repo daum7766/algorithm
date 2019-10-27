7dcf3188503805bfdaf4050fd17c1d4e4361d454
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-10-27 22:44:36 +0900

    Lv2_탑 코드수정

--- 탑(큐,스택 Lv2)/Lv2_Top_Stack.cpp
+++ 탑(큐,스택 Lv2)/Lv2_Top_Stack.cpp
@@ -10,11 +10,9 @@ vector<int> solution(vector<int> heights) {
 	stack<int> st;
 	bool check = true;
 	//리스트를 뒤에서부터 순회하기
-	for (int j = heights.size() - 1; j >= 0; j--)
-	{
+	for (int j = heights.size() - 1; j >= 0; j--) {
 		//자기자신과 비교할필요가 없기때문에 비교하는것의 앞부터 계산
-		for (int i = j - 1; i >= 0; i--)
-		{
+		for (int i = j - 1; i >= 0; i--) {
 			//제일뒤에꺼보다 앞에타워가 큰경우
 			if (heights.at(i) > heights.at(j))
 			{
@@ -27,16 +25,13 @@ vector<int> solution(vector<int> heights) {
 			}
 		}
 		//만약 앞터워가 큰게없다면
+		//스택에 없다고 0을 넣기
 		if (check)
-		{
-			//스택에 없다고 0을 넣기
 			st.push(0);
-		}
 		check = true;
 	}
 	//스택이 빌동안 정답에 푸쉬
-	while (!st.empty())
-	{
+	while (!st.empty()) {
 		answer.push_back(st.top());
 		st.pop();
 	}
@@ -44,15 +39,23 @@ vector<int> solution(vector<int> heights) {
 	return answer;
 }
 
+void print(vector<int> heights, vector<int> answer)
+{
+	vector<int> t = solution(heights);
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << " ";
+		if (answer[i] != t[i])
+		{
+			cout << "틀림" << endl;
+			return;
+		}
+	}
+	cout << "정답" << endl;
+}
+
 int main()
 {
-	vector<int> a = { 6, 9, 5, 7, 4 };
-
-	vector<int> b = solution(a);
-	for (int i = 0; i < b.size(); i++)
-	{
-		cout << b.at(i) << endl;
-	}
+	print({ 6, 9, 5, 7, 4 }, { 0,0,2,2,4 });
 
 	return 0;
 }
