b90318b9c6d1e92cb131a693bcf895382e8254b8
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-11-10 23:58:56 +0900

    Lv2_주식가격 테스트코드 추가

--- 큐,스택/주식가격(큐,스택 Lv2)/Lv2_StockPrice_stack.cpp
+++ 큐,스택/주식가격(큐,스택 Lv2)/Lv2_StockPrice_stack.cpp
@@ -1,29 +1,40 @@
+#include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
+
 vector<int> solution(vector<int> prices) {
 	vector<int> answer;
-	for (int i = 0; i < prices.size() - 1; i++)
-	{
+	//마지막을 제외하고 순회(마지막은 가격변동이 없기때문)
+	for (int i = 0; i < prices.size() - 1; i++) {
 		int count = 0;
-		for (int j = i + 1; j < prices.size(); j++)
-		{
-			//다음거보다 가격이 떨어지지 않았따면
-			if (prices.at(i) <= prices.at(j))
-			{
-				count++;
-			}
-			else//가격이 떨어졌다면
-			{
-				count++;
+		//자기보다 뒤에를 순회하면서 자기보다 가격이 낮으면 반복문을 멈춤
+		for (int j = i + 1; j < prices.size(); j++) {
+			count++;
+			if (prices.at(i) > prices.at(j))
 				break;
-			}
 		}
+		//지속된 시간입력
 		answer.push_back(count);
 	}
 	//제일 마지막꺼 처리
 	answer.push_back(0);
 
 	return answer;
+}
+
+void print(vector<int> prices, vector<int> answer) {
+	vector<int> t = solution(prices);
+	//cout << t << " , ";
+	if (answer == t)
+		cout << "정답" << endl;
+	else
+		cout << "틀림" << endl;
+}
+
+int main() {
+	print({ 1,2,3,2,3 }, { 4,3,1,1,0 });
+	return 0;
 }
