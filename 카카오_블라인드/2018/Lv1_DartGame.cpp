84eb7f397a39dae264fbdd834b910adef04a0422
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-11-15 00:16:03 +0900

    코드수정

--- 카카오_블라인드/2018/Lv1_DartGame.cpp
+++ 카카오_블라인드/2018/Lv1_DartGame.cpp
@@ -7,11 +7,10 @@ using namespace std;
 //다음 문자열 확인함수
 int nextCheck(string& dartResult, int i) {
 	//인덱스가 길이를 넘지않는지 확인
-	if (i + 1 < dartResult.length())
-		switch (dartResult[i + 1]) {
-		case '*':	return 2;	//스타상이라면 2배
-		case '#':	return -1;		//아차상이라면 -1배
-		}
+	if (i + 1 < dartResult.length()) {
+		if (dartResult[i + 1] == '*')	return 2;	//스타상이라면 2배
+		else if (dartResult[i + 1] == '#') return -1;	//아차상이라면 -1배
+	}
 	return 1;	//아무것도 아니라면 1배
 }
 
@@ -25,28 +24,16 @@ int solution(string dartResult) {
 		else if (dartResult[i] == 'T')	Squared = 3;
 		//점수가 확인되었다면
 		if (Squared) {
-			//앞의 숫자 확인하기 1~10;
+			//앞의 숫자 확인하기 0~10;
 			int temp_int = stoi(dartResult.substr(0, i));
 			//다음글자를 확인해서 2배인지 -1배인지 1배인지 확인
 			int mulityply = nextCheck(dartResult, i);
-			//1배가 아니라면 -> 2배 혹은 -1배라면
-			if (mulityply != 1) {
-				// -2배의 경우가 있을 수 있기때문에 다음것을 다시체크
-				int temp = nextCheck(dartResult, i + 1);
-				//둘중하나라도 2배라면 이전값도 2배로 바꾸기
-				if ((mulityply == 2 || temp == 2) && current) 	score[current - 1] *= 2;
-				//다음값이 1배가 아니라면 -> 즉 -2배라면
-				if (temp != 1) {
-					//제한사항7번인 *#이 중첩된경우이다.
-					dartResult = dartResult.substr(i + 3);
-					//-2배로 바꾸어준다
-					mulityply *= temp;
-				}
-				//옵션이 1개이므로 2칸뒤로 이동한다.
-				else	dartResult = dartResult.substr(i + 2);
-			}
+			//1배가 아니라면 -> 2배 혹은 -1배라면 2칸뒤로 이동
+			if (mulityply != 1) 	dartResult = dartResult.substr(i + 2);
 			//옵션이 없으므로 1칸뒤로 이동한다.
 			else	dartResult = dartResult.substr(i + 1);
+			//2배점수였다면 이전점수 또한 2배로 바꿔준다.
+			if (mulityply == 2 && current) 	score[current - 1] *= 2;
 			//현재 다트횟수에 점수를 넣어준다.
 			score[current] = pow(temp_int, Squared) * mulityply;
 			//문자열을 잘랐으므로 인덱스초기화, 다트회수증가,  S D T영역 초기화
