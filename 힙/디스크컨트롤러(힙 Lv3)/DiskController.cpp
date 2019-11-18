9a9bba6dfd6edde0d2ba66c8caf51fd6578a7afd
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-11-18 16:19:23 +0900

    디스크 컨트롤러 문제풀이 수정

--- 힙/디스크컨트롤러(힙 Lv3)/DiskController.cpp
+++ 힙/디스크컨트롤러(힙 Lv3)/DiskController.cpp
@@ -5,10 +5,6 @@
 
 using namespace std;
 
-bool compare(vector<int> a, vector<int> b) {
-	return a.at(0) < b.at(0);
-}
-
 struct cmp {
 	bool operator()(vector<int> a, vector<int> b) {
 		return a.at(1) > b.at(1);
@@ -16,50 +12,48 @@ struct cmp {
 };
 
 int solution(vector<vector<int>> jobs) {
-	int answer = 0;	//결과 저장용 변수
-	int j = 0;	//인덱스 관리용
-	int time = 0;	//시간 체크용
-	sort(jobs.begin(), jobs.end(), compare);
+	//결과 저장용 변수, 인덱스 관리용, 시간체크용
+	int answer = 0, j = 0, time = 0;
+	//시간에 따른 오름차순으로 정렬
+	sort(jobs.begin(), jobs.end());
 	priority_queue<vector<int>, vector<vector<int>>, cmp> pq; //우선순위 큐 min heap
 	//대기열이 없고 우선순위 큐가 빌때까지 반복
 	while (j < jobs.size() || !pq.empty()) {
 		//들어올 대기열이 남아있고, 들어올 대기열이 현재시간보다 작다면
-		if (jobs.size() > j && time >= jobs.at(j).at(0)) {
-			//우선순위 큐에 추가
-			pq.push(jobs.at(j));
-			//인덱스 증가
-			j++;
+		if (jobs.size() > j && time >= jobs[j][0]) {
+			//우선순위 큐에 추가하고 인덱스 증가
+			pq.push(jobs[j++]);
 			//같은시간대에 다른작업이 더들어왔을 수 있으므로 다시 확인
 			continue;
 		}
 		//큐가 비어있지 않다면
 		if (!pq.empty()) {
 			//시간에 이작업이 끝날때까지 걸리는 시간만큼 추가
-			time += pq.top().at(1);
+			time += pq.top()[1];
 			//작업시간에 대기 시간만큼 추가(현재시간 - 들어온 시간)
-			answer += time - pq.top().at(0);
+			answer += time - pq.top()[0];
 			//작업이 끝났으므로 우선순위 큐에서 제거
 			pq.pop();
 		}
-		else {//큐가 비어있다면
-			time++;//시간증가
-		}
+		else //큐가 비어있다면 다음 작업시간으로 넘김
+			time = jobs[j][0];
 	}
 	return answer / jobs.size();//평균을 구해야하므로 총작업한 개수로 나눠줌
 }
 
-int main()
-{
-	//int ajobs[][2] = { {0, 3}, {1, 9}, {2, 6}};
-	int ajobs[][2] = { {1, 4}, {1, 3}, {1, 5}, {2, 4}, {2, 3} ,{3, 1} };
-	vector<vector<int>> jobs;
-	for (int i = 0; i < 6; i++)
-	{
-		vector<int> temp;
-		temp.push_back(ajobs[i][0]);
-		temp.push_back(ajobs[i][1]);
-		jobs.push_back(temp);
-	}
-	cout << solution(jobs) << endl;
+void print(vector<vector<int>> jobs, int answer) {
+	int t = solution(jobs);
+	if (t == answer)
+		cout << "정답" << endl;
+	else
+		cout << "틀림" << endl;
+}
+
+int main() {
+	print({ {0, 3}, {1, 9}, {2, 6} }, 9);
+	print({ {0, 9}, {0, 4}, {0, 5}, {0, 7}, {0, 3} }, 13);
+	print({ {1, 9}, {1, 4}, {1, 5}, {1, 7}, {1, 3} }, 13);
+	print({ {0, 3}, {4, 4}, {5, 3}, {4, 1} }, 3);
+	print({ {0, 3}, {1, 9}, {500, 6} }, 6);
 	return 0;
 }
