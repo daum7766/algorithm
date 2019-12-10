b628b084cc9ecc88b36f3f624e23d7e32bcb0aab
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-12-10 16:50:50 +0900

    섬연결하기, 단속카메라 수정

--- 탐욕법/단속카메라(탐욕법 Lv3)/Lv3_IntermittentCamera.cpp
+++ 탐욕법/단속카메라(탐욕법 Lv3)/Lv3_IntermittentCamera.cpp
@@ -11,38 +11,30 @@ int solution(vector<vector<int>> routes) {
 	//들어온 리스트 정렬
 	sort(routes.begin(), routes.end());
 	//비교를 위해 처음차가 나가는 부분 체크
-	int temp = routes.at(0).at(1);
+	int temp = routes[0][1];
 	//리스트 순회하기
 	for (auto a : routes) {
-		cout << a.at(0) << " , " << a.at(1) << endl;
 		//현재 차가 나가는 부분보다 뒤에 차가 들어온다면
-		if (temp < a.at(0)) {
+		if (temp < a[0]) {
 			//카메라 설치
 			answer++;
 			//나가는 부분 정정
-			temp = a.at(1);
+			temp = a[1];
 		}
 		//현재 차보다 뒤차가 먼저나가면 
-		if (temp >= a.at(1))
-			temp = a.at(1);// 나가는 부분을 뒷차로 수정
+		if (temp >= a[1])	temp = a[1];// 나가는 부분을 뒷차로 수정
 	}
 	return answer;
 }
 
-void print(vector<vector<int>> routes, int answer)
-{
+void print(vector<vector<int>> routes, int answer){
 	int t = solution(routes);
-	cout << t << " , ";
-	if (t == answer)
-		cout << "정답" << endl;
-	else
-		cout << "틀림" << endl;
-
+	if (t == answer)		cout << "정답" << endl;
+	else		cout << "틀림" << endl;
 }
 
 
-int main()
-{
+int main(){
 	print({ {-20,15}, {-14, -5}, {-18, -13}, {-5, -3} }, 2);
 	print({ {-2, -1}, {1, 2}, {-3, 0} }, 2);
 	print({ {0, 0} }, 1);
@@ -50,6 +42,5 @@ int main()
 	print({ {0, 1}, {2, 3}, {4, 5}, {6, 7} }, 4);
 	print({ {-20, 15}, {-14, -5}, {-18, -13}, {-5, -3} }, 2);
 	print({ {-20, 15}, {-20, -15}, {-14, -5}, {-18, -13}, {-5, -3} }, 2);
-
 	return 0;
 }
