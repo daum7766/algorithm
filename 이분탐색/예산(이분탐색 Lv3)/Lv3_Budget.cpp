910cccd99d796e8555d10548de13bb5554d81642
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-12-15 00:02:21 +0900

    이분탐색 코드수정

--- 이분탐색/예산(이분탐색 Lv3)/Lv3_Budget.cpp
+++ 이분탐색/예산(이분탐색 Lv3)/Lv3_Budget.cpp
@@ -7,12 +7,10 @@ using namespace std;
 int solution(vector<int> budgets, int M) {
 	//정답, 최대값, 최솟값, 평균
 	int answer = 0, max = 100000, min = 1, avg;
-	//합계
-	long long sum = 0;
 	while (min <= max) {
 		//상한액을 정할필요가 없는지 체크용
 		bool check = true;
-		sum = 0;
+		long long sum = 0;
 		avg = (max + min) / 2;
 		//순회를 하면서 상한액보다 크다면 평균값으로 바꾸고 저장
 		for (auto a : budgets) {
@@ -20,17 +18,14 @@ int solution(vector<int> budgets, int M) {
 				sum += avg;
 				check = false;
 			}
-			else
-				sum += a;
+			else	sum += a;
 		}
 		//총합이 최대 예산을 넘었다면 최대값을 수정하고 다시 반복
-		if (sum > M)
-			max = avg - 1;
+		if (sum > M)	max = avg - 1;
 		//총합이 최대 예산이 보다 적을때
 		else {
 			//상한액에 한번도 걸리지않았다면 원소중 최댓값 리턴
-			if (check)
-				return *max_element(budgets.begin(), budgets.end());
+			if (check)		return *max_element(budgets.begin(), budgets.end());
 			//그게아니라면 최솟값 바꾸고 다시계산
 			min = avg + 1;
 			answer = avg;
@@ -39,14 +34,10 @@ int solution(vector<int> budgets, int M) {
 	return answer;
 }
 
-void print(vector<int> budgets, int M, int answer)
-{
+void print(vector<int> budgets, int M, int answer){
 	int t = solution(budgets, M);
-	cout << t << " , ";
-	if (t == answer)
-		cout << "정답" << endl;
-	else
-		cout << "틀림" << endl;
+	if (t == answer)	cout << "정답" << endl;
+	else	cout << "틀림" << endl;
 }
 
 int main() {
