aaba3fbfc74b8b98db3937f8d48c3d5bbe565720
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-12-16 11:21:05 +0900

    입국심사 코드수정

--- 이분탐색/입국심사(이분탐색 Lv3)/Lv3_BinarySerch_Immigration.cpp
+++ 이분탐색/입국심사(이분탐색 Lv3)/Lv3_BinarySerch_Immigration.cpp
@@ -15,8 +15,7 @@ long long solution(int n, vector<int> times) {
 		//평균시간 구하기
 		avgTime = ((maxTime + minTime) / 2);
 		//현재 시간으로 돌릴수 잇는 최대인원 구하기
-		for (auto t : times)
-			human += avgTime / t;
+		for (auto t : times)		human += avgTime / t;
 		//최대인원이 총인원보다 크거나 같다면
 		if (n <= human) {
 			//현재 시간을 정답에 저장(나중에 안나오면 리턴해야 해서
@@ -24,28 +23,23 @@ long long solution(int n, vector<int> times) {
 			//최대시간 갱신
 			maxTime = avgTime - 1;
 		}
-		else//총인원보다 적다면 현재보다 시간이커야하므로 최소시간 갱신
-			minTime = avgTime + 1;
+		//총인원보다 적다면 현재보다 시간이커야하므로 최소시간 갱신
+		else	minTime = avgTime + 1;
 		//사람수 초기화
 		human = 0;
 	}
 	return answer;
 }
 
-void print(int n, vector<int> times, long long answer)
-{
+void print(int n, vector<int> times, long long answer){
 	long long t = solution(n, times);
-	cout << t << " , ";
-	if (t == answer)
-		cout << "정답" << endl;
-	else
-		cout << "틀림" << endl;
+	if (t == answer)		cout << "정답" << endl;
+	else		cout << "틀림" << endl;
 }
 
-int main()
-{
+int main(){
+	print(6, { 7, 10 }, 28);
 	print(1, { 2, 2 }, 2);
 	print(1, { 1 }, 1);
-	print(6, { 7, 10 }, 28);
 	return 0;
 }
