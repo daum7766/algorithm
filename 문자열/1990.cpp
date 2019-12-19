102b9110b27daa94b3e224a9d052d7c879512a16
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-12-19 14:46:53 +0900

    1990번 주석추가

--- 문자열/1990.cpp
+++ 문자열/1990.cpp
@@ -7,7 +7,10 @@ int main() {
 	string a;
 	int sum{};
 	cin >> a;
+	//반복문을 이용해서 한글자씩 접근;
+	//char는 -'0'을 하면 숫자가 된다.
 	for (auto t : a) 	sum += t - '0';
+	//sum을 3으로 나눈값이 0이라면 3의 배수이다.
 	if (!(sum % 3))	cout << 1 << endl;
 	else	cout << 0 << endl;
 	return 0;
