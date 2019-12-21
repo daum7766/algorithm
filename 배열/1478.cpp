6b54d0234b7fb86bde1ceab016ddaf93d362df2f
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2019-12-21 20:52:33 +0900

    주석추가

--- 배열/1478.cpp
+++ 배열/1478.cpp
@@ -7,7 +7,9 @@ int main() {
 	cin >> n >> m;
 	for (int j = 0, count = 1; j <= n + m - 2; j++) {
 		for (int i = 0; i < n; i++) {
+			//제일 오른쪽 열부터 시작한다.
 			for (int k = m - 1; k >= 0; k--) {
+				//규칙을 찾으면 m- 1 -k +i를 하면 규칙적인 합이나온다.
 				if (i + (m - 1 - k) == j) {
 					a[i][k] = count++;
 				}
