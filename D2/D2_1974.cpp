2eb4e0da9821171eca2afcc9b665e3748276b8d6
mungto <daum7766@naver.com>
2020-02-05 17:15:32 +0900

    스도쿠배열검증 수정

--- D2/D2_1974.cpp
+++ D2/D2_1974.cpp
@@ -2,42 +2,46 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
 
 using namespace std;
+//D2 1974 스도쿠 배열 검증
 int list[9][9];
-
+//사각형 검사하는 함수
 int nextCheck() {
-	map<int, int> check;
+	//제일윗줄부터 오른쪽으로 진행하며 검사
 	for (int i = 0; i < 9; i += 3) {
 		for (int j = 0; j < 9; j += 3) {
+			int sum[10]{ 0 };
+			//현재 i와j위치에서 +3개씩 검사
 			for (int k = i; k < i + 3; k++) {
 				for (int y = j; y < j + 3; y++) {
-					if (check[list[k][y]])		return 0;
-					check[list[k][y]]++;
+					if (sum[list[k][y]])
+						return 0;
+					sum[list[k][y]]++;
 				}
 			}
-			check.clear();
 		}
 	}
 	return 1;
 }
 
 int solution() {
-	map<int, int> check1, check2;
+	//스도쿠 데이터를 입력받음
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++)
 			cin >> list[i][j];
 	}
+	//가로세로를 돌면서 중복이 있는지 체크
 	for (int i = 0; i < 9; i++) {
+		int sum1[10]{ 0 }, sum2[10]{ 0 };
 		for (int j = 0; j < 9; j++) {
-			if (check1[list[i][j]] || check2[list[j][i]])	return 0;
-			check1[list[i][j]]++;
-			check2[list[j][i]]++;
+			if (sum1[list[i][j]] || sum2[list[j][i]])
+				return 0;
+			sum1[list[i][j]]++;
+			sum2[list[j][i]]++;
 		}
-		check1.clear();
-		check2.clear();
 	}
+	//가로세로에 이상이없다면 3칸짜리 삼각형 검사
 	return nextCheck();
 }
 
