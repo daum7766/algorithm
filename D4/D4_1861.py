9d17b318b5c2590574425a18151d305aa08ccf84
kim yu ki(Mung to) <daum7766@naver.com>
2020-03-03 19:23:03 +0900

    정사각형 방 풀이 수정

--- D4/D4_1861.py
+++ D4/D4_1861.py
@@ -3,51 +3,37 @@
 #상 하 좌 우
 move_list = [(-1,0), (1, 0), (0, -1), (0, 1)]
 
-#이동가능 카운트 세는함수
-def check(i, j):
-    x, y = j, i
-    count = 0
-    while True:
-        #상하좌우로 이동해본다.
-        for dy, dx in move_list:
-            #이동이 불가능하다면 다른걸로 넘어간다.
-            if y+dy <0 or x + dx <0 or y+dy >=N or x + dx >= N:
-                continue
-            #이동이 가능하다면 이동위치를 임시변수에 담고
-            t_y = y+dy
-            t_x = x+dx
-            #이동위치가 현재보다 1큰지 비교한다.
-            if map_list[t_y][t_x] == map_list[y][x]+1:
-                #이동가능하면 count를 증가하고 현재위치 변경
-                count += 1
-                y = t_y
-                x = t_x
-                break
-        else:
-            #4방향을 갈동안 break가 안걸렸다면 이동이 불가능함.
-            break
-        #지금까지 누적된 count 리턴
-    return count
-
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
     map_list = [list(map(int, input().split()))for _ in range(N)]
-    minnum = 0
-    max_count = 0
+    #옆으로 이동이 가능한지 체크하는 변수
+    check_list = [False] * (N*N + 1)
+    minnum = max_count = count = 0
     for i in range(N):
         for j in range(N):
-            #현재 좌표에서 이동가능한 거리계산
-            count = check(i,j)
-            #지금까지 이동거리가 최대이동거리보다 크다면
-            if count > max_count:
-                #이동거리 갱신
+            #상하좌우를 이동해보며 이동이 가능한지 확인한다.
+            for dy, dx in move_list:
+                t_y = i+dy
+                t_x = j+dx
+                #이동이 불가능하다면 다른방향을 확인한다.
+                if t_y <0 or t_x <0 or t_y >=N or t_x >= N:
+                    continue
+                #이동이 가능하다면 자신보다 1이큰지 확인한다.
+                #크다면 정지하고 다음을 확인
+                if map_list[t_y][t_x] == map_list[i][j] + 1:
+                    check_list[map_list[i][j]] = True
+                    break
+    #연결되어있는 배열을 확인한다.
+    for i in range(N*N-1, -1, -1):
+        #연결이 되어있다면 카운트 증가
+        if check_list[i]: count += 1
+        #연결이 끊겼다면 지금까지 누적된 카운트를 체크한다.
+        elif count :
+            #최대 카운트보다 크다면 갱신하고 카운트 초기화
+            if max_count <= count:
                 max_count = count
-                #방의 값을 넣어준다.
-                minnum = map_list[i][j]
-                #카운트가 같다면
-            if count == max_count:
-                #숫자가 작은걸 넣어준다.
-                minnum = min(minnum, map_list[i][j])
-
+                minnum = i+1
+            count = 0
+                
     print("#{} {} {}".format(t, minnum, max_count+1))
