98db414b2d973a8d814769d7437ad0d639d15261
daum7766 <daum7766@naver.com>
2020-03-17 15:18:24 +0900

    GNS 주석 추가

--- D3/D3_1221.py
+++ D3/D3_1221.py
@@ -1,17 +1,16 @@
-import sys
-sys.stdin = open('GNS_test_input.txt')
-
 #D3 1221 GNS 
 
 T = int(input())
 for t in range(1, T+1):
     N = input()
+    #각 개수를 저장하는 딕셔너리
     str_dict = {'ZRO': 0, 'ONE': 0, 'TWO': 0, 'THR': 0, 'FOR': 0, 'FIV': 0, 'SIX': 0, 'SVN': 0, 'EGT': 0, 'NIN': 0}
     str_list = input().split()
     result = ''
     for s in str_list:
+        #각각 카운팅한다.
         str_dict[s] += 1
-    
+    #각 원소가 몇개인지 나오기때문에 앞에서부터 개수만큼 생성한다.
     for key, value in str_dict.items():
         temp = ' '.join([key] * value)
         result += temp + ' '
