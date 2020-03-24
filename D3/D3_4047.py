4bc5a35efc5345f474486d3874bdac3edeb0373f
daum7766 <daum7766@naver.com>
2020-03-24 20:08:01 +0900

    영준이의 카드카운팅 풀이수정

--- D3/D3_4047.py
+++ D3/D3_4047.py
@@ -1,41 +1,16 @@
 #D3 4047 영준이의 카드 카운팅
 #카드 리스트 셋팅
-
-import sys
-sys.stdin = open('sample_input.txt', 'r')
-
-def solution(t):
+for t in range(1, int(input())+1):
     t_str = input()
     t_lst = []
-    cards = {'S': [True for i in range(13)], 'D': [True for i in range(13)], 'H': [True for i in range(13)], 'C' : [True for i in range(13)]}
+    #카드갯수
     counts = {'S': 13, 'D': 13, 'H': 13, 'C' : 13}
-    for i in range(0, len(t_str), 3):
-        t_lst.append(t_str[i:i+3])
-    result = ''
-    for lst in t_lst:
-        if cards[lst[0]][int(lst[1:3])-1]:
-            cards[lst[0]][int(lst[1:3])-1] = False
-            counts[lst[0]] -= 1
-        else: result = 'ERROR'
-    print('#{} '.format(t), end='')
-    if result: print(result)
-    else : print(*counts.values())
-
-def solution2(t):
-    t_str = input()
-    t_lst = []
-    counts = {'S': 13, 'D': 13, 'H': 13, 'C' : 13}
-    for i in range(0, len(t_str), 3): 
-        t_lst.append(t_str[i:i+3])
-        
+    #3칸씩 분리해서 저장
+    for i in range(0, len(t_str), 3): t_lst.append(t_str[i:i+3])
+    #겹치는게 있다면 에러 출력
     if len(set(t_lst)) != len(t_lst): print("#{} ERROR".format(t))
+    #안겹치면 카드깍고 원소 출력
     else:
         for k in t_lst: counts[k[0]] -= 1
         print("#{} ".format(t), end='')
         print(*counts.values())
-
-T = int(input())
-for t in range(1, T+1):
-    t_str = input()[::3]
-    print(t_str)
-    #solution2(t)
