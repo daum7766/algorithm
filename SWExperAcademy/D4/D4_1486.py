efbf444cbda8088c96c07c5d8b5542a00b748551
daum7766 <daum7766@naver.com>
2020-05-30 12:01:03 +0900

    주석수정

--- SWExperAcademy/D4/D4_1486.py
+++ SWExperAcademy/D4/D4_1486.py
@@ -1,6 +1,6 @@
 #D4 1486 장훈이의 높은선반
 
-def bfs(idx, _sum):
+def dfs(idx, _sum):
     global min_num
     #합계가 이미 최소치를 넘어가면 계산안함
     if _sum >= min_num:
@@ -8,13 +8,13 @@ def bfs(idx, _sum):
     #마지막 까지 왔다면
     if idx >= N:
         #선반이상이고 최솟값보다 작을때 값갱신
-        if _sum >= B and _sum < min_num:
+        if _sum >= B:
             min_num = _sum
         return
     visited[idx] = True
-    bfs(idx+1, _sum + height[idx])
+    dfs(idx+1, _sum + height[idx])
     visited[idx] = False
-    bfs(idx+1, _sum)
+    dfs(idx+1, _sum)
 
 
 T = int(input())
@@ -24,5 +24,5 @@ for t in range(1, T+1):
     height = list(map(int, input().split()))
     min_num = 987654321
     visited = [False] * N
-    bfs(0, 0)
+    dfs(0, 0)
     print('#{} {}'.format(t, min_num - B))
