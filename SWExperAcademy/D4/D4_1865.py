83890af87603b43360b0f0f39b4f823ff10142de
daum7766 <daum7766@naver.com>
2020-06-05 23:54:24 +0900

    동철이의 일분배 수정

--- SWExperAcademy/D4/D4_1865.py
+++ SWExperAcademy/D4/D4_1865.py
@@ -5,7 +5,7 @@
 def bfs(idx, answer = 100):
     global max_value
     #값이 계산한 이전값보다 작다면 리턴
-    if max_value != -1 and answer <= max_value: return
+    if answer <= max_value: return
     #위에서 걸러지므로 여기까지 왔다면 그냥 값을 넣는다.
     if idx >= N:
         max_value = answer
@@ -24,7 +24,7 @@ for t in range(1, T+1):
     #람다식을 이용하여 넣을때부터 100으로 나누어 사용
     success_list = [list(map(lambda x : int(x)/100, input().split())) for _ in range(N)]
     use_check = [True for _ in range(N)]
-    max_value = -1
+    max_value = 0
     bfs(0)
     #6자리수로 제한하여 출력
     print('#{} {:6f}'.format(t, max_value))
