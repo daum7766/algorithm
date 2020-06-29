c5fbfc5def70c3d040fe6e37d7eb8e25fe62bcd3
mungto <daum7766@naver.com>
2020-06-29 23:34:43 +0900

    코드수정 조건문 완화

--- HackerRank/Easy/GradingStudents.py
+++ HackerRank/Easy/GradingStudents.py
@@ -1,15 +1,12 @@
 grades_count = 4
-
 grades = [73, 67, 38, 33]
-
 
 def gradingStudents(grades):
     answer = []
     for i in range(grades_count):
         k = grades[i]
-        temp = k
         a = k % 5
         if k >= 38 and a >= 3:
-            temp = k + (5 - a)
-        answer.append(temp)
+            k += 5 - a
+        answer.append(k)
     return answer
