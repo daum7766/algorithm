8a7c607357c579ebf5a0eafd43c678eeb4c1349e
김유기 <daum7766@naver.com>
2020-07-14 09:18:33 +0900

    펠린드롬 수 풀이

---
+++ Baekjoon/Bronze/Level1/1259.py
@@ -0,0 +1,15 @@
+# bronze 1 펠린드롬 수
+
+while True:
+    user_input = input()
+    if user_input == '0':
+        break
+    
+    length = len(user_input)
+    answer = 'yes'
+    for i in range(length//2):
+        if user_input[i] != user_input[length-1-i]:
+            answer = 'no'
+            break
+    
+    print(answer)
