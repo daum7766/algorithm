ed807b1776ff28bc47cba84c569853050a1c4ac9
멍토 <daum7766@naver.com>
2020-12-16 22:35:44 +0900

    refactor: 코드 변경

--- SWExperAcademy/D2/D2_2001.py
+++ SWExperAcademy/D2/D2_2001.py
@@ -1,20 +1,33 @@
 #2001 D2 파리퇴치
 #누적합을 이용한 계산
-T = int(input())
-for t in range(1, T+1):
-    N, M = map(int, input().split())
-    Flys = [list(map(int, input().split())) for _ in range(N)]
-    sum_Flays = [[0 for i in range(N+1)] for j in range(N)]
-    for i in range(N):
-        for j in range(1, N+1):
-            sum_Flays[i][j] = sum_Flays[i][j-1] + Flys[i][j-1]
-    max_sum = 0
-    for i in range(N-M+1):
-        for j in range(M, N+1):
-            current_sum = 0
-            for k in range(M):
-                current_sum += sum_Flays[i+k][j] - sum_Flays[i+k][j-M]
-            max_sum = max(max_sum, current_sum)
 
-            
-    print("#{} {}".format(t, max_sum))
+def data_init():
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    sum_arr = [[0 for _ in range(N+1)] for _ in range(N+1)]
+    
+    for i in range(N):
+        for j in range(N):
+            sum_arr[i][j+1] = arr[i][j] + sum_arr[i][j]
+
+    return N, M, arr, sum_arr
+
+
+def solution():
+    max_data = 0
+    N, M, arr, sum_arr = data_init()
+
+    for y in range(N-M+1):
+        for x in range(N-M+1):
+            data = 0
+            for i in range(M):
+                data += sum_arr[y+i][x+M] - sum_arr[y+i][x]
+            max_data = max(max_data, data)
+
+    return max_data
+
+
+test_case_size = int(input())
+for t in range(test_case_size):
+    answer = solution()
+    print('#{} {}'.format(t+1, answer))
